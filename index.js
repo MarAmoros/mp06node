66d26958eb69f0faa4b654ac772e3598ef6d9da8
MarAmoros <mamoroslopez.cf@iesesteveterradas.cat>
2022-02-15 19:06:13 +0100

    a

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const { text } = require("express");
-const express = require("express");
-const app = express();
+const express = require('express'); // Importar express
+const app = express(); // Asignar express a la variable app
+const port = 8000; // Puerto para express
 
-app.get("/", (req,res) =>{
-
-    res.send(
-    "<h1>Buscador de alumnos: </h1> <input>"
- 
-    )
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+"/cliente.html")
 });
 
-app.listen(8000, () => {
-    console.log("Example app listening port 8000")
+
+app.get('/profesores', (req, res) => {
+    let profesores = ["Profe1", "Profe2"];
+    res.json(JSON.stringify(profesores));
+});
+app.get('/alumnos', (req, res) => {
+    let students = ["Student1", "Student2"];
+    res.json(JSON.stringify(students));
+
+});
+// Crear un servidor web con express el el puerto asignado en la varible port.
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
