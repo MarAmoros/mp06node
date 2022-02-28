03e70254ce9e4a826f820df6bf4dc2a0e37bd77e
MarAmoros <mamoroslopez.cf@iesesteveterradas.cat>
2022-02-28 17:03:10 +0100

    heroku test no security

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 const express = require('express'); // Importar express
 const app = express(); // Asignar express a la variable app
 const port = 8000; // Puerto para express
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://Mar02:holaBuenas@testcluster.abqp2.mongodb.net/school?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/cliente.html")
 });
 
-
 app.get('/profesores', (req, res) => {
-   // Connection URI
-   const uri =
-   "mongodb://localhost:27017/?maxPoolSize=20&w=majority";
- // Create a new MongoClient
- const client = new MongoClient(uri);
 
+ 
+   
+  async function run() {
+      try {
+        await client.connect()
+          const database = client.db("school");
+          const profesores = database.collection("profesores");
 
-     async function run() {
-         try {
-           await client.connect();
-             const database = client.db("school");
-             const profesores = database.collection("profesores");
+          const cursor = await profesores.find()
+          let profAr = await cursor.toArray() //guardas el find en una variable
+          res.json(JSON.stringify(profAr)); // lo envias a /alumnos
 
-             const cursor = await profesores.find()
-             let profAr = await cursor.toArray() //guardas el find en una variable
-             res.json(JSON.stringify(profAr)); // lo envias a /alumnos
-
-         } finally {
-           await client.close();
-         }
-       }
-       run().catch(console.dir);
-    //let profesores = ["Profe1", "Profe2"];
-    //res.json(JSON.stringify(profesores));
+      } finally {
+        await client.close();
+      }
+      
+       
+    }
+    run().catch(console.dir);
+ //let profesores = ["Profe1", "Profe2"];
+ //res.json(JSON.stringify(profesores));
+        
 });
+
 
 app.get('/alumnos', (req, res) => {
     // Connection URI
-    const uri =
-      "mongodb://localhost:27017/?maxPoolSize=20&w=majority";
+
     // Create a new MongoClient
-    const client = new MongoClient(uri);
-
-
         async function run() {
             try {
-              await client.connect();
+              await client.connect()
                 const database = client.db("school");
                 const alumnos = database.collection("alumnos");
 
@@ -55,7 +57,7 @@ app.get('/alumnos', (req, res) => {
                   const cursor = await alumnos.find()
                 let alumAr = await cursor.toArray() //guardas el find en una variable
                 res.json(JSON.stringify(alumAr)); // lo envias a /alumnos
-
+                
             } finally {
               await client.close();
             }
